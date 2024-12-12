1016708525a39a1c310310cf524fc524a262095a
smj5846 <smj5846@naver.com>
2024-12-12 12:32:12 +0900

    질문 모델 생성하기

---
+++ pybo/model.py
@@ -0,0 +1,11 @@
+from pybo import db
+
+# Question 클래스가 db.Model 클래스를 상속받음.
+
+# db.Model 이라는 클래스를 Question이 받아서 새로운 클래스를 생성
+class Question(db.Model):
+    # Column, primary key, nullable, DateTime -> DB에 관한 설정
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
