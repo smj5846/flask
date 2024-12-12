d7fce9435a6b6fd77de231fa14433636ce26f4c5
smj5846 <smj5846@naver.com>
2024-12-12 12:12:29 +0900

    데이터베이스 초기화하기

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -12,11 +12,11 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
 
-    # ORM
+    # ORM -> DB와 연관이 됨
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # 블루프린트
+    # 블루프린트 -> 라우트와 관련이 됨.
     from .views import main_views
     app.register_blueprint(main_views.bp)
 
